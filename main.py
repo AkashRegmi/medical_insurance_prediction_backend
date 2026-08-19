0a30b7096d96a353396a3f94cf801de75727f1ae
Akash Regmi <akashregmi82@gmail.com>
2026-08-19 12:34:40 +0545

    added the cors option

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://medical-insurance-prediction-frontend.onrender.com"
+        "https://medical-insurance-prediction-frontend.onrender.com",
+       " https://medical-insurance-prediction-frontend.onrender.com/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
